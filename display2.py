86237984ca6188a1760a0b6d16890ab234aa8f7a
toothlessG <216097@walshjesuit.org>
2016-02-10 09:52:32 -0500

    Update display2.py

--- display2.py
+++ display2.py
@@ -2,8 +2,12 @@ import requests
 import json
 import csv
 
+print("B1")
+
 name = 'toothlessg'
 matchids = []
+
+print("B2")
 
 #check for key
 def checkforkey(diction,key):
@@ -12,13 +16,14 @@ def checkforkey(diction,key):
     else:
         #print("missing key")
         return 0
-
+print("B3")
 
 #Get ID from name
 idr = requests.get('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/'+name+'?api_key=28f2fa25-e664-4919-bd2e-2e61caf18c6f')
 jsonidr = json.loads(idr.text)
 sumid = str(jsonidr[name]['id'])
 print ("ID=" + sumid)
+print("B4")
 
 #Get Game IDs from recent games
 gamesr = requests.get('https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/'+sumid+'/recent?api_key=28f2fa25-e664-4919-bd2e-2e61caf18c6f')
